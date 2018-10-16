46fdbb2302abb55b339b618766e7fa1a5ad4ef83
Maureen Holland <maureen.holland11@gmail.com>
2018-10-16 16:57:29 -0400

    add methods

--- Greetr.js
+++ Greetr.js
@@ -1,11 +1,97 @@
 // IIFE creates new execution context to avoid conflict with other code
 (function(global, $){
 
-	var Greetr = function(firstName, lastName, language) {
+	var Greetr = function(firstName, lastName, language, level) {
 		return new Greetr.init(firstName, lastName, language);
 	}
 
-	Greetr.prototype = {};
+	// Greetr.init will have access to these variables through closure but the variable is not exposed outside of this function
+	var supportedLangs = ['en', 'fr'];
+
+	var greetings = {
+		en: 'Hello',
+		fr: 'Salut'
+	};
+
+	var formalGreetings = {
+		en: 'Greetings',
+		fr: 'Bonjour'
+	};
+
+	var informalGreetings = {
+		en: 'Hey',
+		fr: 'Coucou'
+	}
+
+	var logMessages = {
+		en: 'Connected',
+		fr: 'Connexion'
+	};
+
+	Greetr.prototype = {
+
+		fullName: function(){
+			return this.firstName + ' ' + this.lastName;
+		},
+
+		validate: function(){
+			if (supportedLangs.indexOf(this.language) === - 1) {
+				throw "Invalid Language"
+			}
+		},
+
+		greeting: function(){
+			return greetings[this.language] + ' ' + this.firstName;
+		},
+
+		formalGreeting: function(){
+			return formalGreetings[this.language] + ' ' + this.fullName();
+		},
+
+		informalGreeting: function(){
+			return informalGreetings[this.language] + ' ' + this.firstName;
+		},
+
+		greet: function(level){
+			var msg;
+
+			switch (level) {
+				case 'informal':
+					msg = this.informalGreeting();
+					break;
+				case 'formal':
+					msg = this.formalGreeting();
+					break;
+				default :
+					msg = this.greeting();
+					break
+			}
+
+			if(console){
+				console.log(msg);
+			}
+
+			// the return allows this method to be chainable
+			return this;
+		},
+
+		log: function(){
+			if(console){
+				console.log(logMessages[this.language] + ': ' + this.fullName())
+			}
+
+			return this;
+		},
+
+		setLang: function(lang){
+			this.language = lang;
+
+			this.validate();
+
+			return this;
+		}
+
+	}; /* end of object literal for Greetr prototype */
 
 	Greetr.init = function(firstName, lastName, language) {
 
@@ -17,6 +103,7 @@
 
 	}
 
+	// Allows object created from Greetr.init to access all properties and methods of Greetr.prototype
 	Greetr.init.prototype = Greetr.prototype;
 
 	global.Greetr = global.G$ = Greetr;
--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 var g = G$('John', 'Doe');
-console.log(g);
+
+console.log(g.fullName());
+g.greet().setLang('fr').greet('formal').greet('informal');
